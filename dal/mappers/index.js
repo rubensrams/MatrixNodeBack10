68b35104bb736c21085faef0f4c69af3e71ebef8
rubensrams <you@example.com>
2020-05-16 22:49:58 -0500

    ok

--- dal/mappers/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    toDbEntity(user) {
-        const { id, usuario, password, nombre, activo, email, foto, google } = user;
-        return { id, password, usuario, nombre, activo, email, foto, google };
-    }
-
-};
